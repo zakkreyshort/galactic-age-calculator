c3be264288c48cb2c040cf401371d72cd66e6b3b
Student <example@example.com>
2020-02-07 11:40:21 -0800

    start on front end, create form, input for age

---
+++ src/main.js
@@ -0,0 +1,14 @@
+  
+import { Human } from '../src/age.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+
+$(document).ready(function(){
+	$("form#galacticAgeForm").submit(function(event){
+		event.preventDefault();
+		
+	});
+})
