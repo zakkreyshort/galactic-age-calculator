2fecdaab6951eb4543314263bc0bb9fff7b1721c
Student <example@example.com>
2020-02-07 12:46:52 -0800

    output for mercury years

--- src/main.js
+++ src/main.js
@@ -5,11 +5,18 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+
+
 // front end
 $(document).ready(function(){
 	$("form#galacticAgeForm").submit(function(event){
 		event.preventDefault();
+		// output earth age 
 		let ogAge = parseInt($("input#galacticAgeInput").val());
 		$("#earthAgeOutput").text(ogAge);
+		let human = new Human(ogAge);
+
+		let mercuryAge = human.calculateMercuryYears(ogAge);
+		$("#mercuryAgeOutput").text(mercuryAge);
 	});
 })
