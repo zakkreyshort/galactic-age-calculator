8e7a82036683b932bd0ce150e6077e2a650a8dea
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-02-21 14:25:02 -0800

    fix venus test

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -86,7 +86,7 @@ describe ('Human', () => {
 	// test successfully returns human years past venus expectancy
 	test('should return number of human years past venus life expectancy', () => {
 		const doodleHuman = new Human(80);
-		expect(doodleHuman.calculateVenusLifeExp()).toEqual("-12.932258064516134")
+		expect(doodleHuman.calculateVenusLifeExp()).toEqual("12.932258064516134years passed LE")
 	});
 
 	// test successfully returns human years past mars expectancy
